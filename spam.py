ccea8065472b4d24d7be28d118188689e8f9ad00
Beryl Kerubo <berylben966@gmail.com>
2024-03-03 17:35:21 +0300

    adding comments

--- spam.py
+++ spam.py
@@ -1,4 +1,6 @@
-spam = int(input())
+# This code snippet is a simple program that takes a number as input
+# and prints a corresponding greeting based on the value of the input.
+spam = int(input()) # The input value is converted to an integer
 if spam == 1:
     print("Hello")
 elif spam == 2:
